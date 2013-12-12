ae1bfdef590645bb0df2e38999227e9ef39c4f96
Andrew Giel <agiel@stanford.edu>
2013-12-12 13:21:09 -0800

    added AUC plotter

---
+++ src/makeAUCvThreshold.py
@@ -0,0 +1,34 @@
+import sklearn.metrics as metrics
+import matplotlib.pyplot as plt
+
+testResults = {
+	1.00: [263, 950, 598, 124],
+	1.01: [265, 926, 622, 122],
+	1.02: [266, 919, 629, 121],
+	1.03: [267, 918, 630, 120],
+	1.05: [267, 887, 661, 120],
+	1.08: [273, 884, 664, 114],
+	1.10: [277, 866, 682, 110],
+	1.20: [290, 839, 709, 97],
+	1.35: [303, 777, 771, 84],
+	1.50: [313, 712, 836, 74]
+}
+
+def calculateAUC(results):
+	TP, TN, FP, FN = results
+	FPR = float(FP)/(FP + TN)
+	TPR = float(TP)/(TP + FN)
+	fpr = [0., FPR, 1.]
+	tpr = [0., TPR, 1.]
+	return metrics.auc(fpr, tpr)
+
+xData = []
+yData = []
+for threshold in testResults:
+	results = testResults[threshold]
+	auc = calculateAUC(results)
+	xData.append(threshold)
+	yData.append(auc)
+
+plt.plot(xData, yData)
+plt.show()
